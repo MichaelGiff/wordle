bb77495470b1a1a5d5246c2789ec868492445e2e
Michael Gifford <mikeyg_1102@icloud.com>
2023-11-17 08:11:53 -0700

    initial commit

--- Wordle2/src/Wordle2/Wordle.java
+++ Wordle2/src/Wordle2/Wordle.java
@@ -16,6 +16,7 @@ public class Wordle {
         int maxGuesses = 6;
         for (int i = 0; i < maxGuesses; i++) {
             System.out.println("\nEnter your guess:");
+            System.out.println(" hello");
             String guess = scanner.next();
 
             ArrayList<Character> correctLetters = new ArrayList<>();
